300f5f96e1c2b183c6a8b22845c536c758355aaf
Thomas Ballinger <thomasballinger@gmail.com>
2011-09-24 20:14:00 -0400

    Made gameEntry attributes properties so they work in templates

--- models.py
+++ models.py
@@ -17,22 +17,26 @@ class GameEntry():
             self.gid = str(gid)
             self.game_link = '/game/text/' + str(gid)
 
+    @property
     def name(self):
-        return r.hget(self.key(), 'name')
+        return r.hget(self.key, 'name')
 
+    @property
     def game(self):
-        return pickle.loads(r.hget(self.key(), 'pickle'))
+        return pickle.loads(r.hget(self.key, 'pickle'))
 
+    @property
     def players(self):
-        return r.hget(self.key(), 'players').split(',')
+        return r.hget(self.key, 'players').split(',')
 
+    @property
     def key(self):
         return 'game_entry:'+self.gid
 
     def set_name_game_players(self, name, gameObj, players):
-        r.hset(self.key(), 'name', name)
-        r.hset(self.key(), 'players', ','.join(players))
-        r.hset(self.key(), 'pickle', pickle.dumps(gameObj))
+        r.hset(self.key, 'name', name)
+        r.hset(self.key, 'players', ','.join(players))
+        r.hset(self.key, 'pickle', pickle.dumps(gameObj))
         #for player in players:
             #r.sadd(self.players_key, player)
             #if not r.sismember('users', player):
@@ -40,7 +44,7 @@ class GameEntry():
             #r.sadd('users:'+player, self.game_id)
 
     def update_game(self, game):
-        r.hset(self.key(), 'pickle', pickle.dumps(game))
+        r.hset(self.key, 'pickle', pickle.dumps(game))
 
 def get_gameEntries(player=None):
     if player:
